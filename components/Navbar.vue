f199aea2ecbc23f4fcf29bff83a24496aedce768
Saravenpi <saravenpi@tuta.io>
2025-01-17 00:48:03 +0100

    💄 Black font

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col gap-3 md:gap-0 md:flex-row md:items-center justify-between w-full p-6 md:py-6 md:px-10">
     <a href="/" class="flex flex-row items-center">
       <img src="/logo.png" alt="Kaktuse Logo" class="h-[3rem] md:h-[3rem]" />
-      <h1 class="text-[2rem] font-bold ml-2">Kaktuse.</h1>
+      <h1 class="text-[2rem] font-black ml-2">Kaktuse.</h1>
     </a>
 
     <div class="flex flex-row items-center space-x-6">
