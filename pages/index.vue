17efc08ee704459dff1e9e158d17e88729d78d5a
Saravenpi <saravenpi@tuta.io>
2025-01-16 23:29:44 +0100

    💄 Responsive UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-start p-10 gap-6 relative">
-    <h1 class="text-5xl font-extrabold">Kaktuse.</h1>
-    <div class="flex flex-col absolute bottom-0 right-0 p-10">
-      <h2 class="text-3xl">Contact</h2>
-      <span class="flex items-center text-2xl gap-2">
-        <Icon name="uil:envelope" /> <a href="mailto:contact@kaktuse.net" class="underline">contact@kaktuse.net</a>
-      </span>
+  <div class="h-screen flex flex-col justify-center items-start p-10 relative">
+    <h1 class="text-[4rem] md:text-[9rem] font-black">Kaktuse.</h1>
+    <p class="text-2xl md:text-4xl font-light mt-2">Innovative Web Development Solutions</p>
+    <div class="flex flex-col absolute bottom-0 right-0 p-6 md:p-10">
+      <a href="mailto:contact@kaktuse.net" class="hover:underline flex items-center text-5xl md:text-4xl gap-2">
+        <Icon name="uil:envelope" />
+        <h1 class="hidden md:flex">contact@kaktuse.net</h1>
+      </a>
     </div>
   </div>
 </template>
