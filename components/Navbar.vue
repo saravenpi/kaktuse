5cf946d43758dee1f21701c36426a23eb52cbc35
Saravenpi <saravenpi@tuta.io>
2025-01-17 01:22:34 +0100

    ✨ Active link style

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="flex flex-col gap-3 md:gap-0 md:flex-row md:items-center justify-between w-full p-6 md:py-6 md:px-10">
-    <a href="/" class="flex flex-row items-center">
+    <router-link to="/" class="flex flex-row items-center">
       <img src="/logo.png" alt="Kaktuse Logo" class="h-[3rem] md:h-[3rem]" />
       <h1 class="text-[2rem] font-black ml-2">Kaktuse.</h1>
-    </a>
+    </router-link>
 
     <div class="flex flex-row items-center space-x-6">
-      <a href="/projects" class="text-xl font-medium hover:underline">Projects</a>
-      <a href="/about" class="text-xl font-medium hover:underline">About</a>
-      <a href="/contact" class="text-xl font-medium hover:underline">Contact</a>
+      <router-link to="/projects" class="text-xl font-medium hover:border-b-2 hover:border-white"
+        active-class="border-b-2 border-white">Projects</router-link>
+      <router-link to="/about" class="text-xl font-medium hover:border-b-2 hover:border-white"
+        active-class="border-b-2 border-white">About</router-link>
+      <router-link to="/contact" class="text-xl font-medium hover:border-b-2 hover:border-white"
+        active-class="border-b-2 border-white">Contact</router-link>
     </div>
   </div>
 </template>
