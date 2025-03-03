0fd50611874f23b39ddc054949c245987b759262
Saravenpi <saravenpi@tuta.io>
2025-03-03 14:24:00 +0100

    ✨Added icons

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[#F97315]/30 border-2 border-[#F97315] px-3 py-1 rounded-xl mx-2 my-1 text-[#F97315]">
+  <div class="bg-[#F97315]/30 border-2 border-[#F97315] px-3 py-1 rounded-xl text-[#F97315]">
     <slot />
   </div>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -7,14 +7,24 @@
         <h1 class="text-4xl font-bold mb-8 font-gambarino">Last Projects</h1>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <div v-for="(project, index) in projects" :key="project.id" class="project-card p-6 rounded-xl shadow-lg"
+          <div v-for="(project, index) in projects" :key="project.id"
+            class="flex flex-col project-card p-6 rounded-xl shadow-lg justify-between gap-4"
             :style="{ animationDelay: `${index * 0.2}s` }">
-            <h2 class="text-3xl font-semibold mb-2">{{ project.title }}</h2>
-            <p class="text-xl">{{ project.description }}</p>
-            <div class="flex flex-wrap mt-4">
-              <Badge v-for="tech in project.technologies" :key="tech">
-                {{ tech }}
-              </Badge>
+            <div>
+              <Icon :name="project.icon" class="text-4xl text-center mb-3 text-description" />
+              <h2 class="text-3xl font-semibold mb-2">{{ project.title }}</h2>
+              <p class="text-xl text-description">{{ project.description }}</p>
+            </div>
+            <div>
+              <h2 class="text-xl my-3 text-description font-bold flex items-center gap-2">
+                <Icon name="solar:code-square-bold-duotone" />
+                Languages/Frameworks
+              </h2>
+              <div class="flex flex-wrap gap-3">
+                <Badge v-for="tech in project.technologies" :key="tech">
+                  {{ tech }}
+                </Badge>
+              </div>
             </div>
           </div>
         </div>
@@ -29,9 +39,39 @@ export default {
   data() {
     return {
       projects: [
-        { id: 1, title: 'Disti.life', description: 'Disti.life is a comprehensive ERP solution tailored for distilleries, integrating advanced AI technologies to optimize production processes, manage inventory, and enhance decision-making through predictive analytics.', technologies: ['Vue.js', 'Node.js', 'AI', 'PostegreSQL', 'Docker'] },
-        { id: 2, title: 'Solais Intranet', description: 'Solais is a robust intranet platform designed for a solar company, facilitating seamless internal communication, document management, and collaboration among teams, while providing tools for project tracking and resource allocation.', technologies: ['Angular', 'ExpressJS', 'MongoDB', 'Typescript'] },
-        { id: 3, title: 'Marcel', description: 'Marcel is an innovative gamified organisation app that transforms task management into an engaging experience, encouraging productivity and teamwork through game-like elements and rewards, suitable for both personal and professional use.', technologies: ['Svelte', 'Pocketbase', 'Typescript', 'GoLang'] },
+        {
+          id: 1,
+          title: 'Disti.life',
+          description: 'Disti.life is a comprehensive ERP solution tailored for distilleries, integrating advanced AI technologies to optimize production processes, manage inventory, and enhance decision-making through predictive analytics.', technologies: ['Vue.js', 'Node.js', 'AI', 'PostegreSQL', 'Docker'], icon: 'solar:bottle-bold-duotone'
+        },
+        {
+          id: 2,
+          title: 'Solais Intranet',
+          description: 'Solais is a robust intranet platform designed for a solar company, facilitating seamless internal communication, document management, and collaboration among teams, while providing tools for project tracking and resource allocation.',
+          technologies: ['Angular', 'ExpressJS', 'MongoDB', 'Typescript'],
+          icon: 'solar:sun-2-bold-duotone'
+        },
+        {
+          id: 3,
+          title: 'Marcel',
+          description: 'Marcel is an innovative gamified organisation app that transforms task management into an engaging experience, encouraging productivity and teamwork through game-like elements and rewards, suitable for both personal and professional use.',
+          technologies: ['Svelte', 'Pocketbase', 'Typescript', 'GoLang'],
+          icon: 'solar:checklist-minimalistic-bold-duotone'
+        },
+        {
+          id: 4,
+          title: 'OffOn',
+          description: 'OffOn is a dynamic e-commerce platform specialized in smartphone sales and repairs, combining a sleek, user-friendly interface with powerful inventory and service management tools to streamline operations and enhance the customer experience.',
+          technologies: ['Vue', 'Nuxt', 'Directus', 'Typescript'],
+          icon: 'solar:monitor-smartphone-bold-duotone'
+        },
+        {
+          id: 5,
+          title: 'Brain.h',
+          description: 'Brain.h is a lightweight C library designed for building and training neural networks, offering a straightforward API and efficient performance, making it an ideal choice for developers seeking simplicity and control in AI projects.',
+          technologies: ['C'],
+          icon: 'solar:database-bold-duotone'
+        },
       ]
     }
   }
